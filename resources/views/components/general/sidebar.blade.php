948023700134b52b5e9d29fd1ba6dcc56484cf72
rizqullahyusufrafly <rizqullahyusufrafly@gmail.com>
2025-08-06 07:08:52 +0000

    CHORE : search blade header and sidebar cleaning

--- resources/views/components/general/sidebar.blade.php
+++ resources/views/components/general/sidebar.blade.php
@@ -159,42 +159,6 @@
                     <!--end:Menu link-->
                 </div>
                 <!--end:Menu item-->
-                <!--begin:Menu item-->
-                <div class="menu-item">
-                    <!--begin:Menu link-->
-                    <a class="menu-link" href="https://preview.keenthemes.com/metronic8/demo42/layout-builder.html">
-                        <span class="menu-icon">
-                            <i class="ki-outline ki-abstract-13 fs-2"></i>
-                        </span>
-                        <span class="menu-title">Layout Builder</span>
-                    </a>
-                    <!--end:Menu link-->
-                </div>
-                <!--end:Menu item-->
-                <!--begin:Menu item-->
-                <div class="menu-item">
-                    <!--begin:Menu link-->
-                    <a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs" target="_blank">
-                        <span class="menu-icon">
-                            <i class="ki-outline ki-abstract-26 fs-2"></i>
-                        </span>
-                        <span class="menu-title">Documentation</span>
-                    </a>
-                    <!--end:Menu link-->
-                </div>
-                <!--end:Menu item-->
-                <!--begin:Menu item-->
-                <div class="menu-item">
-                    <!--begin:Menu link-->
-                    <a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/getting-started/changelog" target="_blank">
-                        <span class="menu-icon">
-                            <i class="ki-outline ki-code fs-2"></i>
-                        </span>
-                        <span class="menu-title">Changelog v8.2.6</span>
-                    </a>
-                    <!--end:Menu link-->
-                </div>
-                <!--end:Menu item-->
             </div>
             <!--end::Menu-->
         </div>
@@ -240,78 +204,7 @@
                 <!--end::Menu item-->
                 <!--begin::Menu separator-->
                 <div class="separator my-2"></div>
-                <!--end::Menu separator-->
-                <!--begin::Menu item-->
-                <div class="menu-item px-5">
-                    <a href="account/overview.html" class="menu-link px-5">My Profile</a>
-                </div>
                 <!--end::Menu item-->
-                <!--begin::Menu item-->
-                <div class="menu-item px-5">
-                    <a href="apps/projects/list.html" class="menu-link px-5">
-                        <span class="menu-text">My Projects</span>
-                        <span class="menu-badge">
-                            <span class="badge badge-light-danger badge-circle fw-bold fs-7">3</span>
-                        </span>
-                    </a>
-                </div>
-                <!--end::Menu item-->
-                <!--begin::Menu item-->
-                <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="right-end" data-kt-menu-offset="-15px, 0">
-                    <a href="#" class="menu-link px-5">
-                        <span class="menu-title">My Subscription</span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <!--begin::Menu sub-->
-                    <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="account/referrals.html" class="menu-link px-5">Referrals</a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="account/billing.html" class="menu-link px-5">Billing</a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="account/statements.html" class="menu-link px-5">Payments</a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="account/statements.html" class="menu-link d-flex flex-stack px-5">Statements 
-                            <span class="ms-2 lh-0" data-bs-toggle="tooltip" title="View your statements">
-                                <i class="ki-outline ki-information-5 fs-5"></i>
-                            </span></a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu separator-->
-                        <div class="separator my-2"></div>
-                        <!--end::Menu separator-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <div class="menu-content px-3">
-                                <label class="form-check form-switch form-check-custom form-check-solid">
-                                    <input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications" />
-                                    <span class="form-check-label text-muted fs-7">Notifications</span>
-                                </label>
-                            </div>
-                        </div>
-                        <!--end::Menu item-->
-                    </div>
-                    <!--end::Menu sub-->
-                </div>
-                <!--end::Menu item-->
-                <!--begin::Menu item-->
-                <div class="menu-item px-5">
-                    <a href="account/statements.html" class="menu-link px-5">My Statements</a>
-                </div>
-                <!--end::Menu item-->
-                <!--begin::Menu separator-->
-                <div class="separator my-2"></div>
-                <!--end::Menu separator-->
                 <!--begin::Menu item-->
                 <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
                     <a href="#" class="menu-link px-5">
